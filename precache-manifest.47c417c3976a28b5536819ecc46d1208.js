d31751d42d7c46c64a99be99238f80b44b79f9a3
aod1411 <nothingbutgood@comcast.net>
2022-01-17 12:34:18 -0500

    Updates

---
+++ precache-manifest.47c417c3976a28b5536819ecc46d1208.js
@@ -0,0 +1,146 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a70da47424fc846ecb4ea645b1816aac",
+    "url": "/aod1411/aidaportfolio2022/index.html"
+  },
+  {
+    "revision": "3f2f83772c08591982fa",
+    "url": "/aod1411/aidaportfolio2022/static/css/2.b97dddfb.chunk.css"
+  },
+  {
+    "revision": "2ef19147b1962c40e3ba",
+    "url": "/aod1411/aidaportfolio2022/static/css/main.d7a23f7a.chunk.css"
+  },
+  {
+    "revision": "3f2f83772c08591982fa",
+    "url": "/aod1411/aidaportfolio2022/static/js/2.3a2efa6c.chunk.js"
+  },
+  {
+    "revision": "332bf1b0d236b7fc5455596e6356764c",
+    "url": "/aod1411/aidaportfolio2022/static/js/2.3a2efa6c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ef19147b1962c40e3ba",
+    "url": "/aod1411/aidaportfolio2022/static/js/main.e0786b3a.chunk.js"
+  },
+  {
+    "revision": "ef745ff7370c3e326504",
+    "url": "/aod1411/aidaportfolio2022/static/js/runtime-main.7f01bee1.js"
+  },
+  {
+    "revision": "1a3a635ebceee80b0179a233cac99d7c",
+    "url": "/aod1411/aidaportfolio2022/static/media/05hw.1a3a635e.gif"
+  },
+  {
+    "revision": "3c00369bcba85bc0f126ffed8bb88207",
+    "url": "/aod1411/aidaportfolio2022/static/media/AA.3c00369b.JPG"
+  },
+  {
+    "revision": "891e9d3ded495d64a9c7c5f6f528605f",
+    "url": "/aod1411/aidaportfolio2022/static/media/DnD.891e9d3d.PNG"
+  },
+  {
+    "revision": "f9fb3cb3f9704368a64023210e5149f0",
+    "url": "/aod1411/aidaportfolio2022/static/media/FitnessTracker.f9fb3cb3.PNG"
+  },
+  {
+    "revision": "c215bd2ce47de56577db8b0d2a90009f",
+    "url": "/aod1411/aidaportfolio2022/static/media/NT.c215bd2c.png"
+  },
+  {
+    "revision": "bdb9968f4d6e8015262b2ba3be803e7b",
+    "url": "/aod1411/aidaportfolio2022/static/media/SM.bdb9968f.PNG"
+  },
+  {
+    "revision": "ffded2381d25dd906406d6e07f91dea5",
+    "url": "/aod1411/aidaportfolio2022/static/media/bs.ffded238.svg"
+  },
+  {
+    "revision": "3707da3c6a394c7ee2523273f8d98a25",
+    "url": "/aod1411/aidaportfolio2022/static/media/css.3707da3c.svg"
+  },
+  {
+    "revision": "67af05e12311bac06327b43ca31e166b",
+    "url": "/aod1411/aidaportfolio2022/static/media/earth.67af05e1.jpg"
+  },
+  {
+    "revision": "757a1e505556a411682eedf9471569e1",
+    "url": "/aod1411/aidaportfolio2022/static/media/express.757a1e50.svg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/aod1411/aidaportfolio2022/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "acf3dcb7ff752b5296ca23ba2c7c2606",
+    "url": "/aod1411/aidaportfolio2022/static/media/fontawesome-webfont.acf3dcb7.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/aod1411/aidaportfolio2022/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/aod1411/aidaportfolio2022/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/aod1411/aidaportfolio2022/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "0ff1a6f08182be4aa63a0b54da1b9e57",
+    "url": "/aod1411/aidaportfolio2022/static/media/gh.0ff1a6f0.svg"
+  },
+  {
+    "revision": "b38ff61aa327fbdc24f7b1dad7d04d1a",
+    "url": "/aod1411/aidaportfolio2022/static/media/heroku.b38ff61a.svg"
+  },
+  {
+    "revision": "676e8188f003ed5d4882a0d0f378dad3",
+    "url": "/aod1411/aidaportfolio2022/static/media/html.676e8188.svg"
+  },
+  {
+    "revision": "b9165292217db0a65a0c18c4e2f970d8",
+    "url": "/aod1411/aidaportfolio2022/static/media/ida.b9165292.JPG"
+  },
+  {
+    "revision": "05acfdb568b3df49ad31355b19495d4a",
+    "url": "/aod1411/aidaportfolio2022/static/media/ionicons.05acfdb5.woff"
+  },
+  {
+    "revision": "24712f6c47821394fba7942fbb52c3b2",
+    "url": "/aod1411/aidaportfolio2022/static/media/ionicons.24712f6c.ttf"
+  },
+  {
+    "revision": "2c2ae068be3b089e0a5b59abb1831550",
+    "url": "/aod1411/aidaportfolio2022/static/media/ionicons.2c2ae068.eot"
+  },
+  {
+    "revision": "c037dbbc0e6790f30e824a50010df5fb",
+    "url": "/aod1411/aidaportfolio2022/static/media/ionicons.c037dbbc.svg"
+  },
+  {
+    "revision": "f935b988635bb10498a83f5f012ce214",
+    "url": "/aod1411/aidaportfolio2022/static/media/js.f935b988.svg"
+  },
+  {
+    "revision": "c2a864c96dbfc449ec45dd7ac4438628",
+    "url": "/aod1411/aidaportfolio2022/static/media/mdb.c2a864c9.svg"
+  },
+  {
+    "revision": "9de16c0c2c9ba2a9049933f1b9ee84d6",
+    "url": "/aod1411/aidaportfolio2022/static/media/modern.9de16c0c.png"
+  },
+  {
+    "revision": "b337a2fb0b1289e6757e632dc766ff6b",
+    "url": "/aod1411/aidaportfolio2022/static/media/node.b337a2fb.svg"
+  },
+  {
+    "revision": "4009eef3b3e291f296e71962c70895c5",
+    "url": "/aod1411/aidaportfolio2022/static/media/react.4009eef3.svg"
+  },
+  {
+    "revision": "84df9b821f685f8d067b8333665b7bb3",
+    "url": "/aod1411/aidaportfolio2022/static/media/weatherDashboard.84df9b82.PNG"
+  }
+]);
